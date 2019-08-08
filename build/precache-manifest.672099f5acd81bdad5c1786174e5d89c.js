956159b7726b8b6de6d19e5940569e4e01f47064
Injo Baik <baikinjo@Baik-MacBook-Pro-13.local>
2019-08-08 14:50:41 -0700

    push

---
+++ build/precache-manifest.672099f5acd81bdad5c1786174e5d89c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe4673787ed637e4e49b8590a786d22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2649f17dd3e1596cb19c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2649f17dd3e1596cb19c",
+    "url": "/static/js/main.2efee23b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Header from './components/header/header'
+import About from './components/about/about'
+import Resume from './components/resume/resume'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Resume />
+        <Contact />
+        <Footer />
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
