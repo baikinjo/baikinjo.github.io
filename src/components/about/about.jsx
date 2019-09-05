8eb4136359009bef0e69313a8830ff5745cae5a4
Injo Baik <baikinjo.28@gmail.com>
2019-09-04 22:51:20 -0700

    updated resume, source code button

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,12 +3,12 @@ export default class About extends Component {
   render() {
     return (
       <React.Fragment>
-        <section id="about">
-          <div className="row">
-            <div className="three columns">
-              <img className="profile-pic" src="images/profilepic.jpg" alt="" />
+        <section id='about'>
+          <div className='row'>
+            <div className='three columns'>
+              <img className='profile-pic' src='images/profilepic.jpg' alt='' />
             </div>
-            <div className="nine columns main-col">
+            <div className='nine columns main-col'>
               <h2>About Me</h2>
               <p>
                 I am a <strong>full stack web developer</strong>, experienced in
@@ -18,22 +18,22 @@ export default class About extends Component {
                 web application with testable code and containerization of
                 virtual development environment using Docker.
               </p>
-              <div className="row">
-                <div className="columns contact-details">
+              <div className='row'>
+                <div className='columns contact-details'>
                   <h2>Contact Details</h2>
-                  <p className="address">
+                  <p className='address'>
                     <span>Injo Baik</span>
                     <br />
                     <span>baikinjo.28@gmail.com</span>
                   </p>
                 </div>
-                <div className="columns download">
+                <div className='columns download'>
                   <p>
                     <a
-                      href="https://drive.google.com/open?id=1tTOrAXmqMATHCx8QfZmnY4RfFQLZFobU"
-                      className="button"
+                      href='https://drive.google.com/open?id=11K1Dqqn69C8KFq9oAXSRXQ7TWURP3UeK'
+                      className='button'
                     >
-                      <i className="fa fa-download" />
+                      <i className='fa fa-download' />
                       Download Resume
                     </a>
                   </p>
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,27 +3,27 @@ export default class Portfolio extends Component {
   render() {
     return (
       <React.Fragment>
-        <section id="portfolio">
-          <div className="row">
-            <div className="twelve columns collapsed">
+        <section id='portfolio'>
+          <div className='row'>
+            <div className='twelve columns collapsed'>
               <h1>Check Out Some of My Works.</h1>
               {/* portfolio-wrapper */}
               <div
-                id="portfolio-wrapper"
-                className="bgrid-quarters s-bgrid-thirds cf"
+                id='portfolio-wrapper'
+                className='bgrid-quarters s-bgrid-thirds cf'
               >
-                <div className="columns portfolio-item">
-                  <div className="item-wrap">
-                    <a href="#modal-01" title>
-                      <img alt="" src="images/expensify.png" />
-                      <div className="overlay">
-                        <div className="portfolio-item-meta">
+                <div className='columns portfolio-item'>
+                  <div className='item-wrap'>
+                    <a href='#modal-01' title>
+                      <img alt='' src='images/expensify.png' />
+                      <div className='overlay'>
+                        <div className='portfolio-item-meta'>
                           <h5>Expensify</h5>
                           <p>Web Application</p>
                         </div>
                       </div>
-                      <div className="link-icon">
-                        <i className="icon-plus" />
+                      <div className='link-icon'>
+                        <i className='icon-plus' />
                       </div>
                     </a>
                   </div>
@@ -35,13 +35,13 @@ export default class Portfolio extends Component {
             {/* twelve columns end */}
             {/* Modal Popup
 	      --------------------------------------------------------------- */}
-            <div id="modal-01" className="popup-modal mfp-hide">
+            <div id='modal-01' className='popup-modal mfp-hide'>
               <img
-                className="scale-with-grid"
-                src="images/expensify.png"
-                alt=""
+                className='scale-with-grid'
+                src='images/expensify.png'
+                alt=''
               />
-              <div className="description-box">
+              <div className='description-box'>
                 <h4>Expensify</h4>
                 <p>
                   Manage expenses, add expense, edit or delete expense.
@@ -49,16 +49,17 @@ export default class Portfolio extends Component {
                   description, or sorted by date or amount - Google account
                   needed to login to the app.
                 </p>
-                <span className="categories">
-                  <i className="fa fa-tag" />
+                <span className='categories'>
+                  <i className='fa fa-tag' />
                   React, Redux, Firebase, Babel, Webpack, Scss
                 </span>
               </div>
-              <div className="link-box">
-                <a href="https://expensify-baik.herokuapp.com/dashboard">
+              <div className='link-box'>
+                <a href='https://expensify-baik.herokuapp.com/dashboard'>
                   Go to App
                 </a>
-                <a className="popup-modal-dismiss">Close</a>
+                <a href='https://github.com/baikinjo/expensify'>Source Code</a>
+                <a className='popup-modal-dismiss'>Close</a>
               </div>
             </div>
             {/* modal-01 End */}
